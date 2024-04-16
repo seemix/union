c77338f83e24fc2db933906c6c7471e2fa327095
seemix <91602641+seemix@users.noreply.github.com>
2024-04-16 12:24:15 +0300

    small fixings 1.001

--- src/app/components/Main/BottomWidgets/BottomWidgets.tsx
+++ src/app/components/Main/BottomWidgets/BottomWidgets.tsx
@@ -13,20 +13,20 @@ const BottomWidgets = () => {
         <div className={css.widgets_wrapper}>
             <div className={css.widget_card}>
                 <Link href={'/page?id=111876'} className={css.link}>
-                    <LuChurch size={'3em'}/>
+                    <LuChurch size={'3em'} color={'var(--green-hex)'}/>
                     <h3>Контакты церквей</h3>
                 </Link>
             </div>
             <div className={css.widget_card}>
                 <Link href={'page?id=11'} className={css.link}>
-                    <LiaBibleSolid size={'3em'}/>
+                    <LiaBibleSolid size={'3em'} color={'var(--gray-hex)'}/>
                     <h3>Библия за год</h3>
                 </Link>
                 <p>{passage}</p>
             </div>
             <div className={css.widget_card}>
                 <Link href={'/page?id=45477'} className={css.link}>
-                    <MdOutlineOnlinePrediction size={'3em'}/>
+                    <MdOutlineOnlinePrediction size={'3em'} color={'var(--red-hex)'}/>
                     <h3>Церковь он-лайн</h3>
                 </Link>
             </div>
