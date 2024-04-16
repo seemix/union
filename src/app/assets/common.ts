2756d1d36e5e7116675df1fd099d2a9ccfd27b21
seemix <91602641+seemix@users.noreply.github.com>
2024-04-16 14:14:59 +0300

    social fixings & outsideClick

--- src/app/assets/common.ts
+++ src/app/assets/common.ts
@@ -1,2 +1,5 @@
 export const graphqlUrl = 'https://baptist.org.md/ru/index.php?graphql';
 export const baseURL = 'https://baptist.org.md/ru/?rest_route=/wp/v2/';
+export const faceBookUrl = 'https://www.facebook.com/baptist.md';
+export const youtubeUrl = 'https://www.youtube.com/@baptistmd';
+export const instagramUrl = 'https://www.instagram.com/baptist.md/';
--- src/app/components/SocialIcons/SocialIcons.tsx
+++ src/app/components/SocialIcons/SocialIcons.tsx
@@ -3,14 +3,15 @@ import Link from 'next/link';
 import { FaFacebookF } from 'react-icons/fa6';
 import { FaInstagram, FaYoutube } from 'react-icons/fa';
 
+import { faceBookUrl, instagramUrl, youtubeUrl } from '@/app/assets/common';
 import css from './SocialIcons.module.css';
 
 const SocialIcons = () => {
     return (
         <div className={css.social_icons}>
-            <Link href={''}> <FaFacebookF size={'1.6em'}/></Link>
-            <Link href={''}> <FaInstagram size={'1.7em'}/></Link>
-            <Link href={''}> <FaYoutube size={'1.8em'}/></Link>
+            <Link href={faceBookUrl}> <FaFacebookF size={'1.6em'}/></Link>
+            <Link href={instagramUrl}> <FaInstagram size={'1.7em'}/></Link>
+            <Link href={youtubeUrl}> <FaYoutube size={'1.8em'}/></Link>
         </div>
     );
 };
