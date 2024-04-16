135444405b12fc146a4285560e5d4bd4ac866045
seemix <91602641+seemix@users.noreply.github.com>
2024-04-16 18:35:22 +0300

    social fixings & outsideClick 2.00

--- src/app/components/Main/getSlides.ts
+++ src/app/components/Main/getSlides.ts
@@ -3,7 +3,11 @@ import { IMappedCategoryPost } from '@/app/category/types';
 import { mappedCategoryPosts } from '@/app/category/mapper';
 
 export const getSlides = async (categoryId: number, count: number, excerptLength: number = 1): Promise<IMappedCategoryPost[]> => {
-    const response = await fetch(baseURL + `posts&categories=${categoryId}&page=1&per_page=${count}`);
+    const response = await fetch(baseURL + `posts&categories=${categoryId}&page=1&per_page=${count}`, {
+        next: {
+            revalidate: 60
+        }
+    });
     const data = await response.json();
     return mappedCategoryPosts(data, excerptLength);
 };
