0baf05f72895b8677d28d129d3d285f64b65cfe7
seemix <91602641+seemix@users.noreply.github.com>
2024-04-19 02:52:35 +0300

    blurimages

--- src/app/components/MyImage/MyImage.tsx
+++ src/app/components/MyImage/MyImage.tsx
@@ -10,23 +10,23 @@ const MyImage = async ({ img }: { img: string }) => {
     const checkRemoteImage = async (imagePath: string) => {
         try {
             const response = await fetch(imagePath, { method: 'HEAD' });
-            if(response.ok) return imagePath;
+            if (response.ok) return imagePath;
         } catch (e) {
             return fallBackImage.src;
         }
 
     };
     const buffer = await fetch(img).then(async (res) => {
-       return Buffer.from(await res.arrayBuffer());
+        return Buffer.from(await res.arrayBuffer());
     });
-   const { base64 } = await getPlaiceholder(buffer);
+  //  const { base64 } = await getPlaiceholder(buffer);
     return (
         <div className={css.picture_wrapper}>
             {<Image src={await checkRemoteImage(img) as string} alt={img} fill
                     className={`${css.picture}`}
                     sizes={'(max-width: 1920px) 25vw'}
-                   placeholder={'blur'}
-                    blurDataURL={base64}
+                   // placeholder={'blur'}
+                   // blurDataURL={base64}
             />}
         </div>
     );
