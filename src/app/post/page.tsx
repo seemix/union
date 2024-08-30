4593bf163af6f9fd32fe226acebabda29ed65cc4
seemix <91602641+seemix@users.noreply.github.com>
2024-08-30 15:42:47 +0300

    SEO optimization 1.0

--- src/app/post/page.tsx
+++ src/app/post/page.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Metadata } from 'next';
 import Link from 'next/link';
 
 import { baseURL } from '@/app/assets/common';
@@ -8,23 +9,42 @@ import { contentTransformer } from '@/app/common/functions/contentTransformer';
 import { ContentRender, DateViews } from '@/app/components';
 import { postMapper } from '@/app/post/mapper';
 import { markText } from '@/app/common//functions/markText';
-
 import css from './post.module.css';
 
-const Page = async ({ searchParams }: { searchParams: { id: string, link: string, query?: string } }) => {
+interface ISearchParams {
+    id: string,
+    link: string,
+    query?: string
+}
+
+const getData = async (searchParams: ISearchParams) => {
     const response = await fetch(baseURL + 'posts/' + searchParams.id + '&_embed=wp:term', {
-       // cache: 'no-cache',
+        // cache: 'no-cache',
         next: {
             revalidate: 10
         }
     });
     const rawPost: IRawPost = await response.json();
-    const post = postMapper(rawPost);
+    return postMapper(rawPost);
+
+};
+export const generateMetadata = async ({ searchParams }: { searchParams: ISearchParams }): Promise<Metadata> => {
+    const post = await getData(searchParams);
+    return {
+        title: post.title,
+        description: post.content.split('.')[0],
+        openGraph: {
+            images: post.fimg_url
+        },
+    };
+};
+const Page = async ({ searchParams }: { searchParams: ISearchParams }) => {
+
+    const post = await getData(searchParams);
     if (searchParams.query) {
         post.content = markText(post.content, searchParams.query);
     }
     const slides = imageParser(post.content);
-
     return (
         <div className={'main'}>
             <div className={css.categories_wrapper}>
